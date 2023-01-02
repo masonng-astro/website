4d2883a36c33c8ae604709e3725b7f7db55a0538
masonng-astro <masonng@mit.edu>
2023-01-02 11:31:14 -0500

    Adding collapsibles

--- assets/js/util.js
+++ assets/js/util.js
@@ -62,6 +62,22 @@
 				id = $this.attr('id'),
 				config;
 
+		// Collapsible.
+		var coll = document.getElementsByClassName("collapsible");
+		var i;
+
+		for (i = 0; i < coll.length; i++) {
+		  coll[i].addEventListener("click", function() {
+		    this.classList.toggle("active");
+		    var content = this.nextElementSibling;
+		    if (content.style.maxHeight){
+		      content.style.maxHeight = null;
+		    } else {
+		      content.style.maxHeight = content.scrollHeight + "px";
+		    } 
+		  });
+		}
+
 		// Config.
 			config = $.extend({
 
